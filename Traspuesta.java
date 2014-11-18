3cb5620ae82ad49011458777a7e8aaad10cb0618
sankosk <sakoxh@live.com>
2014-11-18 23:29:49 +0100

    añadida clase para trasposicion

---
+++ Traspuesta.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Traspuesta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Traspuesta
+{
+    private int[][] resultado;
+    
+    public Traspuesta(int iTam, int jTam)
+    {
+        this.setResultado(new int[iTam][jTam]);
+    }
+    
+    public int[][] getResultado()
+    {
+        return resultado;
+    }
+    
+    private void setResultado(int[][] resultado)
+    {
+        if(resultado != null){
+            this.resultado = resultado;
+        }
+    }
+    
+    public int[][] mTrasposicion(int[][] matriz)
+    {
+        for(int i=0; i<matriz.length; i++)
+        {
+            for(int j=0; j<matriz[0].length; j++)
+            {
+                resultado[i][j] = matriz[j][i];
+            }
+        }
+       
+        return resultado;
+    }
+}
